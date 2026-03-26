2a31f67ccdd8ef96a904266042b5d0cf74715b76
许力允 <980144007@qq.com>
2026-03-26 16:46:22 +0800

    新增BbCube正方体组件

---
+++ src/common/components/Bb3DCube/Bb3DCube.vue
@@ -0,0 +1,181 @@
+<template>
+  <div
+    class="cm-3d-text-container"
+    @mousemove="handleMouseMove"
+    @touchstart="handleTouchStart"
+    @touchmove="handleTouchMove"
+    @touchend="handleTouchEnd"
+  >
+    <div class="scene">
+      <div
+        class="cube"
+        :style="{
+          transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
+          width: cubeSize,
+          height: cubeSize,
+          '--translate-z': translateZ,
+          fontSize: fontSize
+        }"
+      >
+        {{ text }}
+        <div
+          class="face"
+          v-for="i in 6"
+          :key="i"
+        >
+          <span class="face-text">{{ text }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup name="Bb3DCube">
+import { ref, computed } from "vue";
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: "",
+  },
+  size: {
+    type: String,
+    default: "200px",
+  },
+  fontSize: {
+    type: String,
+    default: "1.5em",
+  },
+  textColor: {
+    type: String,
+    default: "#ffffff",
+  },
+});
+
+const rotateX = ref(-30);
+const rotateY = ref(45);
+
+const cubeSize = computed(() => props.size);
+const translateZ = computed(() => {
+  const num = parseInt(props.size);
+  return `${num / 2}px`;
+});
+
+let touchStartX = 0;
+let touchStartY = 0;
+let isTouching = false;
+
+const handleMouseMove = (e) => {
+  rotateX.value = -30 + (e.clientY / window.innerHeight - 0.5) * 60;
+  rotateY.value = 45 + (e.clientX / window.innerWidth - 0.5) * 60;
+};
+
+const handleTouchStart = (e) => {
+  isTouching = true;
+  touchStartX = e.touches[0].clientX;
+  touchStartY = e.touches[0].clientY;
+};
+
+const handleTouchMove = (e) => {
+  if (!isTouching) return;
+  e.preventDefault();
+  const {
+    touches: [{clientX, clientY}]
+  } = e;
+  const touchX = clientX;
+  const touchY = clientY;
+
+  const deltaX = touchX - touchStartX;
+  const deltaY = touchY - touchStartY;
+
+  rotateY.value += deltaX * 0.5;
+  rotateX.value -= deltaY * 0.5;
+
+  touchStartX = touchX;
+  touchStartY = touchY;
+};
+
+const handleTouchEnd = () => {
+  isTouching = false;
+};
+</script>
+<style lang="less" scoped>
+.cm-3d-text-container {
+  display: flex;
+  flex-direction: column;
+  padding: 50px;
+
+  .scene {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    perspective: 1800px;
+  }
+
+  .cube {
+    position: relative;
+    transform-style: preserve-3d;
+    transition: transform 0.1s ease-out;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    font-weight: bold;
+    .face {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      opacity: 0.8;
+      backface-visibility: visible;
+      cursor: pointer;
+      transform-style: preserve-3d;
+
+      .face-text {
+        position: absolute;
+        z-index: 10;
+        pointer-events: none;
+        user-select: none;
+        transform: translateZ(1px);
+        background: v-bind(textColor);
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+        text-fill-color: transparent;
+      }
+
+      &:nth-child(1) {
+        background-color: rgba(0, 255, 0, 0.9);
+        transform: translateZ(var(--translate-z));
+      }
+
+      &:nth-child(2) {
+        background-color: rgba(122, 250, 154, 0.9);
+        transform: rotateY(180deg) translateZ(var(--translate-z));
+      }
+
+      &:nth-child(3) {
+        background-color: rgba(255, 255, 0, 0.9);
+        transform: rotateY(90deg) translateZ(var(--translate-z));
+      }
+
+      &:nth-child(4) {
+        background-color: rgba(128, 184, 247, 0.9);
+        transform: rotateY(-90deg) translateZ(var(--translate-z));
+      }
+
+      &:nth-child(5) {
+        background-color: rgba(248, 148, 248, 0.9);
+        transform: rotateX(90deg) translateZ(var(--translate-z));
+      }
+
+      &:nth-child(6) {
+        background-color: rgba(0, 255, 255, 0.9);
+        transform: rotateX(-90deg) translateZ(var(--translate-z));
+      }
+    }
+  }
+}
+</style>
--- src/pages/Mine/Index.vue
+++ src/pages/Mine/Index.vue
@@ -1,65 +1,13 @@
 <template>
   <div 
     class="page-mine-container" 
-    @mousemove="handleMouseMove"
-    @touchstart="handleTouchStart"
-    @touchmove="handleTouchMove"
-    @touchend="handleTouchEnd"
   >
-    <div class="scene">
-      <div
-        class="cube"
-        :style="{ transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)` }"
-      >
-        <div class="face" v-for="i in 6" :key="i">{{ i }}</div>
-      </div>
-    </div>
+    我的
   </div>
 </template>
 
 <script setup name="Mine">
-import { ref } from "vue";
 
-const faceOpacity = ref(0.6);
-
-const rotateX = ref(-30);
-const rotateY = ref(45);
-
-let touchStartX = 0;
-let touchStartY = 0;
-let isTouching = false;
-
-const handleMouseMove = (e) => {
-  rotateX.value = -30 + (e.clientY / window.innerHeight - 0.5) * 60;
-  rotateY.value = 45 + (e.clientX / window.innerWidth - 0.5) * 60;
-};
-
-const handleTouchStart = (e) => {
-  isTouching = true;
-  touchStartX = e.touches[0].clientX;
-  touchStartY = e.touches[0].clientY;
-};
-
-const handleTouchMove = (e) => {
-  if (!isTouching) return;
-  e.preventDefault();
-  
-  const touchX = e.touches[0].clientX;
-  const touchY = e.touches[0].clientY;
-  
-  const deltaX = touchX - touchStartX;
-  const deltaY = touchY - touchStartY;
-  
-  rotateY.value += deltaX * 0.5;
-  rotateX.value -= deltaY * 0.5;
-  
-  touchStartX = touchX;
-  touchStartY = touchY;
-};
-
-const handleTouchEnd = () => {
-  isTouching = false;
-};
 </script>
 
 <style lang="less" scoped>
@@ -70,68 +18,5 @@ const handleTouchEnd = () => {
   display: flex;
   flex-direction: column;
   background-color: #000000;
-
-  .scene {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    perspective: 1800px;
-  }
-
-  .cube {
-    position: relative;
-    width: 200px;
-    height: 200px;
-    transform-style: preserve-3d;
-    transition: transform 0.1s ease-out;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    .face {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 30px;
-      font-weight: bold;
-      color: white;
-      backface-visibility: visible;
-      
-      &:nth-child(1) {
-        background-color: rgba(255, 0, 0, v-bind(faceOpacity));
-        transform: translateZ(100px);
-      }
-
-      &:nth-child(2) {
-        background-color: rgba(0, 255, 0, v-bind(faceOpacity));
-        transform: rotateY(180deg) translateZ(100px);
-      }
-
-      &:nth-child(3) {
-        background-color: rgba(0, 0, 255, v-bind(faceOpacity));
-        transform: rotateY(90deg) translateZ(100px);
-      }
-
-      &:nth-child(4) {
-        background-color: rgba(255, 255, 0, v-bind(faceOpacity));
-        transform: rotateY(-90deg) translateZ(100px);
-      }
-
-      &:nth-child(5) {
-        background-color: rgba(255, 0, 255, v-bind(faceOpacity));
-        transform: rotateX(90deg) translateZ(100px);
-      }
-
-      &:nth-child(6) {
-        background-color: rgba(0, 255, 255, v-bind(faceOpacity));
-        transform: rotateX(-90deg) translateZ(100px);
-      }
-    }
-  }
 }
 </style>
